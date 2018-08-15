d5173539bf796f3b77568c903562d11b767388eb
lekhiem06 <42398379+lekhiem06@users.noreply.github.com>
2018-08-15 14:19:05 +0700

    Create new array
    Create new array

--- array.js
+++ array.js
@@ -1,1 +1,8 @@
+const family = [
+  { name: 'An', age: 37 },
+  { name: 'Chau', age: 6 },
+  { name: 'Khiem', age: 11 },
+  { name: 'Thu', age: 37 },
+];
 
+const name = family.map(pepole => pepole.name);
